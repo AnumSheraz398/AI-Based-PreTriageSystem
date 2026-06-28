9e98435bc6a650848623a8a130d51f1312780f9c
AnumSheraz398 <168107636+AnumSheraz398@users.noreply.github.com>
2026-06-28 19:35:47 +0500

    Updated CORS Policy

--- backend/main.py
+++ backend/main.py
@@ -24,6 +24,8 @@ app.add_middleware(CORSMiddleware,
     allow_origins=[
         "http://localhost:3000","http://localhost:5173",
         "http://localhost:5174","http://127.0.0.1:5173","http://127.0.0.1:5174",
+        "https://ai-based-pretriage-system.vercel.app",
+        "https://ai-based-pre-triage-system.vercel.app",
     ],
     allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
 )
